275e148fffb738b70313cd8686af34dbdda53ffc
Xavier Berger <berger.xavier@gmail.com>
2014-03-22 23:36:59 +0100

    Update install and run script

--- store/rpimonitor/run.sh
+++ store/rpimonitor/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-xdg-open http://127.0.0.1:8888/
+LINE1="<b>RPi-Monitor</b> is correctly installed.\n"
+LINE2="You can reach its web interface by browsing:\n"
+LINE3=$(ip addr | perl -ne '/(\d+\.\d+\.\d+\.\d+)/ and  print "  <a href=\"http://$1:8888/\">http://$1:8888/</a>\\n"')
+#echo $LINE1
+#echo $LINE2
+#echo $LINE3
+zenity --info --text="$LINE1$LINE2$LINE3"
