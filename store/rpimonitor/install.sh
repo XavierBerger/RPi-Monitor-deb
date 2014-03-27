1710a25babc1180be000ab79fc6aff28f6243033
Xavier Berger <berger.xavier@gmail.com>
2014-03-27 18:20:36 +0000

    Update install and uninstall scripts for the store

--- store/rpimonitor/install.sh
+++ store/rpimonitor/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo aptitude -y install librrds-perl libhttp-daemon-perl libjson-perl libipc-sharelite-perl libfile-which-perl shellinabox
 sudo dpkg -i rpimonitor_2.6-1_all.deb
+sudo apt-get update && sudo service rpimonitor update
