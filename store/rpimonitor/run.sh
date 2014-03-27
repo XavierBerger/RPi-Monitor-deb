340224bbdc47c557564ef9d0b1d8de49ae04438e
Xavier Berger <berger.xavier@gmail.com>
2014-03-27 22:32:11 +0100

    Update store's run script

--- store/rpimonitor/run.sh
+++ store/rpimonitor/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 LINE1="<b>RPi-Monitor</b> is correctly installed.\n"
 LINE2="You can reach its web interface by browsing:\n"
-LINE3=$(ip addr | perl -ne '/(\d+\.\d+\.\d+\.\d+)/ and  print "  <a href=\"http://$1:8888/\">http://$1:8888/</a>\\n"')
-#echo $LINE1
-#echo $LINE2
-#echo $LINE3
-zenity --info --text="$LINE1$LINE2$LINE3"
+LINE3=$(ip addr | perl -ne '/(\d+\.\d+\.\d+\.\d+)/ and  print "  - http://$1:8888/\\n"')
+LINE4="Note: <b>RPi-Monitor</b> is a Web2.0 application requiring javascript.\n"
+LINE5="It has successfly tested with Chrome and Firefox but is not compatible with Midori."
+zenity --info --text="$LINE1$LINE2$LINE3$LINE4$LINE5"
